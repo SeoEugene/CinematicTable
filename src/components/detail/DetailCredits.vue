8126c3ce1478e52ab05806dfc6425d75dc8704da
coconutpalmtreeisland <65150534+SeoEugene@users.noreply.github.com>
2024-01-15 15:07:46 +0900

    24.01.15

--- src/components/detail/DetailCredits.vue
+++ src/components/detail/DetailCredits.vue
@@ -3,16 +3,9 @@
     <h3>감독</h3>
     <ul>
       <li>
-        <img
-          v-if="movieCredits.crew[0].profile_path"
-          :src="getPostPath(movieCredits.crew[0].profile_path)"
-          :alt="movieCredits.crew[0].name"
-        />
-        <img
-          v-else
-          src="../../assets/img/noneimg.png"
-          :alt="movieCredits.crew[0].name"
-        />
+        <img v-if="movieCredits.crew[0].profile_path" :src="getPostPath(movieCredits.crew[0].profile_path)"
+          :alt="movieCredits.crew[0].name" />
+        <img v-else src="../../assets/image/noneimg.png" :alt="movieCredits.crew[0].name" />
         <div class="name">{{ movieCredits.crew[0].name }}</div>
       </li>
     </ul>
@@ -21,12 +14,8 @@
     <h3>영화배우</h3>
     <ul>
       <li v-for="actors in movieCredits.cast.slice(0, 10)" :key="actors.id">
-        <img
-          v-if="actors.profile_path"
-          :src="getPostPath(actors.profile_path)"
-          :alt="actors.name"
-        />
-        <img v-else src="../../assets/img/noneimg.png" :alt="actors.name" />
+        <img v-if="actors.profile_path" :src="getPostPath(actors.profile_path)" :alt="actors.name" />
+        <img v-else src="../../assets/image/noneimg.png" :alt="actors.name" />
         <div class="name">{{ actors.name }}</div>
       </li>
     </ul>
